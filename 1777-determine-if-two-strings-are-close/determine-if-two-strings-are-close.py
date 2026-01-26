ceef74c508d5d607dd37850eaf594105edea9fbe
RAJASHEKHAR GUNTAKA <rajareddyofficial@gmail.com>
2026-01-26 13:47:15 -0600

    Time: 74 ms (95.86%) | Memory: 20.8 MB (7.88%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -7,8 +7,7 @@ class Solution:
         if set(word1) != set(word2):
             return False
 
-        if len(word1) == len(word2) and set(word1) == set(word2) and \
-            (sorted(Counter(word1).values()) == sorted(Counter(word2).values())) :
+        if (sorted(Counter(word1).values()) == sorted(Counter(word2).values())) :
             return True
         else: 
             return False
