4d41cecc136d23a281c5bec2b9e491d5103c1dd8
RAJASHEKHAR GUNTAKA <rajareddyofficial@gmail.com>
2026-01-29 01:24:03 -0600

    Time: 222 ms (10.02%) | Memory: 20.6 MB (17.56%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -4,7 +4,7 @@ class Solution:
         stack = []
         i = 0
         while i < len(s):
-            if s[i] == "*":
+            if s[i] == "*" and stack:
                 stack.pop()
             else:
                 stack.append(s[i])
