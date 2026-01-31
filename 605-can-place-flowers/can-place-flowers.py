ae658b7caefeab7d436f0b4fcdafb4d3ccab22df
RAJASHEKHAR GUNTAKA <rajareddyofficial@gmail.com>
2026-01-30 19:06:55 -0600

    Time: 4 ms (87.18%) | Memory: 19.8 MB (5.06%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -4,6 +4,8 @@ class Solution:
         flowerbed = [0]+flowerbed+[0]
         count = 0
 
+        if n == 0: return True
+
         for i in range(1, len(flowerbed)-1):
 
             if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
@@ -14,6 +16,4 @@ class Solution:
                 return True
 
         return False
-
-
 
